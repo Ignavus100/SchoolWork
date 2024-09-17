8f997c427cf387ec92d725e54f2c5ce92147dfc6
OLI <olicass100@gmail.com>
2024-09-17 10:57:07 +0100

    schoolwork

---
+++ DataStructures.py
@@ -0,0 +1,10 @@
+names = [[["Bert", "Bill", "Betty"],
+        ["Josh", "Kelsie", "Sami"],
+        ["Hyunwoo", "Oli", "Billy"]], 
+        [["Bert", "Bill", "Betty"],
+        ["Josh", "Kelsie", "Sami"],
+        ["Hyunwoo", "Oli", "Billy"]],
+        [["Bert", "Bill", "Betty"],
+        ["Josh", "Kelsie", "Sami"],
+        ["Hyunwoo", "Oli", "Billy"]]]
+print(names[0][2][2])
