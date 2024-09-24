278e684585575b595a8dacf6579f455b818a32ec
OLI <olicass100@gmail.com>
2024-09-24 15:05:02 +0100

    updating the OOP task with talking pokemon

--- OOP.py
+++ OOP.py
@@ -8,5 +8,8 @@ class Pokemon():
         self.df = df
     def Damage(self, damage):
         self.hp -= damage
+    def Talk(self):
+        print(self.name)
 newp = Pokemon("Bulbasaur", "Grass", 100, 97, 67, 43)
 newp.Damage(10)
+newp.Talk()
