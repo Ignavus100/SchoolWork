f8f0651a9fffcfd953c60d44117d2a0bfd4ef6de
OLI <olicass100@gmail.com>
2024-09-27 20:31:55 +0100

    updates to the pokemon games

--- OOP.py
+++ OOP.py
@@ -1,12 +1,13 @@
 import random
 class Pokemon():
-    def __init__(self, name, ptype, hp, spd, att, df):
+    def __init__(self, name, ptype, hp, spd, att, df, inventory):
         self.name = name
         self.ptype = ptype
         self.hp = hp
         self.spd = spd
         self.att = att
         self.df = df
+        self.inventory = inventory
     def Damage(self, damage):
         self.hp -= damage
     def Talk(self):
@@ -19,14 +20,26 @@ class Pokemon():
     def Fade(self):
         if self.hp <= 0:
             print(self.name + " has faded")
-    def Eat(self, food):
-        self.hp += 10
+    def Eat(self):
+        fruit = ["berrys", "apples", "peaches"]
+        for i in range(3):
+            #try:
+            print(str(i+1) + ". you have " + str(self.inventory.count(i+1)) + " " + fruit[i])
+            #except:
+                #pass
+        choice = int(input(">"))
+        try:
+            self.inventory.pop(choice)
+        except:
+            pass
+        val = choice * 10
+        self.hp += val
 
 #OC Creating pokemon
-Bulbasaur = Pokemon("Bulbasaur", "Grass", 45, 45, 49, 49)
-Ivysaur = Pokemon("Ivysaur", "Grass", 60, 60, 62, 63)
-Venusaur = Pokemon("Venusaur", "Grass", 80, 80, 82, 83)
-MVenusaur = Pokemon("MVenusaur", "Grass", 80, 80, 100, 123)
+Bulbasaur = Pokemon("Bulbasaur", "Grass", 45, 45, 49, 49, [])
+Ivysaur = Pokemon("Ivysaur", "Grass", 60, 60, 62, 63, [])
+Venusaur = Pokemon("Venusaur", "Grass", 80, 80, 82, 83, [])
+MVenusaur = Pokemon("MVenusaur", "Grass", 80, 80, 100, 123, [])
 pokemon = [Bulbasaur, Ivysaur, Venusaur, MVenusaur]
 
 def main():
@@ -47,9 +60,9 @@ def main():
 ''')
         action = int(input(">"))
         if action == 1:
-            choice.Eat(1)
+            choice.Eat()
         elif action == 2:
-            choice.Attack(other, 10)
+            choice.Attack(other, random.randint(5, 15))
         elif action == 3:
             Lost = True
             temp = False
@@ -58,5 +71,27 @@ def main():
             temp = False
             print("invalid input")
         if temp:
-            other.Attack(choice, 10)
+            rnum = random.randint(1, 3)
+            choice.inventory.append(rnum)
+            if rnum == 1:
+                print("you have picked up a berry. +(10hp)")
+            elif rnum == 2:
+                print("you have picked up an apple. (+20hp)")
+            else:
+                print("you have picked up a peach. (+30hp)")
+            other.Attack(choice, random.randint(5, 15))
+        if other.hp <= 0 or choice.hp <= 0:
+            choice.Fade()
+            other.Fade()
+            GameOver = True
+    if Lost:
+        print("YOU LOST!!!")
+        choice = input("Do you want to play again? y/n \n").lower()
+        if choice == "y":
+            main()
+    else:
+        print("You Won!!")
+        choice = input("Do you want to play again? y/n \n").lower()
+        if choice == "y":
+            main()
 main()
