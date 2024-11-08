6842ef448f6b7a16fce53b90df6575533776cee1
OLI <olicass100@gmail.com>
2024-11-08 10:52:23 +0000

    editing ritangle program

--- ritangle.py
+++ ritangle.py
@@ -5,7 +5,9 @@ triangle = list("TRIANGLE")
 while val < 100001:
     val += i + 1
     if val % 90==0:
-        if ritangle[int((val/90)%8)] == triangle[i%8]:
+        if ritangle[int(((val/90)%8) - 1)] == triangle[(i%8) - 1]:
+            print(ritangle[int(((val/90)%8) -1)])
+            print(triangle[i%8])
             print(val)
 
     
