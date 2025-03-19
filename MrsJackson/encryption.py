b77d0bdb2cbbd7cc164790c1022df5a7ab32592e
OLI <olicass100@gmail.com>
2025-03-19 14:33:04 +0000

    test

--- MrsJackson/encryption.py
+++ MrsJackson/encryption.py
@@ -1,14 +1,40 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet = list(alphabet)
-reverse = "zyxwvutsrqponmlkjihgfedcba"
-reverse = list(reverse)
-substring = "Dvoo wlmv, blf'ev hloevw xrksvi xszoovmtv mfnyvi gdl. Gsrh gbkv lu xrksvi rh z hkvxrzo hfyhgrgfgrlm xrksvi pmldm zh Zgyzhs, zmw rh nzwv yb ivevihrmt gsv zokszyvg zmw ivkozxrmt gsv ozhg ovggvi drgs gsv urihg, gsv kvmfogrnzgv ovggvi drgs gsv hvxlmw, zmw hl lm. Rg rh xzoovw Zgyzhs yvxzfhv rg dzh lirtrmzoob wlmv drgs gsv Svyivd zokszyvg, zmw rg gllp rgh mznv uiln gsv urihg zmw ozhg, zmw hvxlmw zmw kvmfogrnzgv ovggvih rm gszg zokszyvg. Hfyhgrgfgrlm xrksvih ivkozxv gsv ovggvih lu gsv nvhhztv drgs lgsvi ovggvih. Xzvhzi hsrug zmw Zgyzhs ziv uzriob hrnkov hfyhgrgfgrlm xrksvih gl xizxp, yvxzfhv lmxv blf'ev nzwv z uvd tfvhhvh rg'h vzhb gl hklg gsv hbhgvn gszg szh yvvm fhvw gl wvxrwv lm gsv hfyhgrgfgrlmh. Gsv mvcg nvhhztv dlm'g yv jfrgv hl vzhb gl xizxp, hl ivnvnyvi gl ollp uli kfmxgfzgrlm, hrmtov ovggvih zmw hslig dliwh. Urmzoob, gsv pvbdliw gl zxxvhh gsv gsriw xszoovmtv rh gvzxzpv."
-substring = substring.lower()
-new = ""
-for i in substring:
+dictionary = {
+    "a":"e",
+    "b":"h",
+    "c":"",
+    "d":"",
+    "e":"",
+    "f":"t",
+    "g":"",
+    "h":"",
+    "i":"",
+    "j":"",
+    "k":"",
+    "l":"",
+    "m":"",
+    "n":"",
+    "o":"",
+    "p":"",
+    "q":"",
+    "r":"",
+    "s":"",
+    "t":"",
+    "u":"",
+    "v":"",
+    "w":"",
+    "x":"",
+    "y":"",
+    "z":"",
+}
+newtext = ""
+cyphertext = "FBALLQUQMNLQUQSLQUQKWQWMOFBQUANKIQDIQUALKRGKRALYOWARQWQVQIC QDRUAQFQDKUQLZYQDRAQDIOLFONAWWKDIAVNABKWVQDKIFKUAWQLFBMOSBAQ NLYGNKFANWIMUVQNARLLQUQWFMWBAAVFBAKNWKUKLQNKFYFMFBAIQUALGQW WMMDNAIMSDKHARFBAYGANAKDILORARKDFBASADOWIQUALOWQLMDSGKFBQLVQI QKDFBAWYWFAUQDQFONQAMJLKDDQAOWWMUAMDAADINYVFKDSQUAWWQSAIQDU QCARAINYVFKMDUMNARKJJKIOLFZYIBMMWKDSODOWOQLGMNRWQDRNAUMXKDS VODIFOQFKMDQDRFBAWVQIKDSZAFGAADGMNRWWAAKJYMOIQDGMNCMOFFBAUAF BMROWARJMNADINYVFKDSFBKWUAWWQSAFBKDCQZMOFWAEOADIAWKJYMOSAFW FOIC".lower()
+for i in cyphertext:
     try:
-        new += (reverse[alphabet.index(i)])
+        if dictionary[i] in "qwertyuiopasdfghjklzxcvbnm":
+            newtext += dictionary[i]
+        else:
+            newtext += "_"
     except:
-        new += " "
+        newtext += i
 
-print(new)
+print(newtext)
